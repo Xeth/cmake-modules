553a1e18200d00f0589726f68f10549371a0587a
Victor Bucataru <ping@devktor.com>
2016-02-29 00:18:20 +0200

    windows hints

--- FindJsonCPP.cmake
+++ FindJsonCPP.cmake
@@ -6,20 +6,29 @@ else  (JSONCPP_INCLUDE_DIR AND JSONCPP_LIBRARY)
 
     set(JsonCPP_FIND_QUIETLY FALSE)
 
+if (WIN32)
+    find_path (JSONCPP_ROOT_DIR
+      HINTS $ENV{PROGRAMFILES}/jsoncpp
+      NAMES jsoncpp/json/value.h
+      PATHS ENV JSONCPPROOT
+      DOC "jsoncpp root directory")
+else ()
     find_path (JSONCPP_ROOT_DIR
       NAMES jsoncpp/json/value.h
       PATHS ENV JSONCPPROOT
       DOC "jsoncpp root directory")
+endif()
 
     find_path (JSONCPP_INCLUDE_DIR
       NAMES json/value.h
-      HINTS ${JSONCPP_ROOT_DIR}
+      HINTS ${JSONCPP_ROOT_DIR} ${JSONCPP_ROOT_DIR}/include
       PATH_SUFFIXES jsoncpp
       DOC "jsoncpp include directory")
 
     find_library(
         JSONCPP_LIBRARY
         NAMES jsoncpp
+        HINTS ${JSONCPP_ROOT_DIR} ${JSONCPP_ROOT_DIR}/lib
     )
 
 endif (JSONCPP_INCLUDE_DIR AND JSONCPP_LIBRARY)
