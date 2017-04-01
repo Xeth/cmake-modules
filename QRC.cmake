53f50234fa3b050663a2dc21b58f372462ecfed8
Victor Bucataru <ping@devktor.com>
2017-04-01 16:58:56 +0300

    qrc functions refactoring

--- QRC.cmake
+++ QRC.cmake
@@ -27,39 +27,29 @@ function(GENERATE_QRC RCC_NAME ROOT_PATH DIRS)
 endfunction(GENERATE_QRC)
 
 
-function(PARSE_QRC RESOURCE_FILES DIR EXT QRC PARSER)
 
-    file(GLOB DIR_PATH "resources/${DIR}")
-    file(GLOB QRC_PATH "resources")
-    set(QRC_PATH "${QRC_PATH}/${QRC}.qrc")
 
-    list(REMOVE_ITEM ${RESOURCE_FILES} ${DIR_PATH})
-    list(REMOVE_ITEM ${RESOURCE_FILES} ${QRC_PATH})
-    file(GLOB_RECURSE FILES "${PROJECT_SOURCE_DIR}/resources/${DIR}/*.${EXT}")
+function(PARSE_RESOURCES_TARGET TARGET_NAME RESOURCE_FILES IN_DIR OUT_DIR EXT PARSER)
 
+    file(GLOB IN_DIR ${IN_DIR})
+    file(GLOB OUT_DIR ${OUT_DIR})
+    file(GLOB_RECURSE FILES "${IN_DIR}/*.${EXT}")
     set(GENERATED_FILES "")
 
     foreach(FILE_PATH ${FILES})
-        string(REPLACE ${DIR_PATH} "" FILE_NAME "${FILE_PATH}")
-        set(PARSED_NAME ${PROJECT_BINARY_DIR}/resources/${DIR}${FILE_NAME})
+        string(REPLACE ${IN_DIR} "" FILE_NAME "${FILE_PATH}")
+        set(PARSED_NAME ${OUT_DIR}${FILE_NAME})
         list(APPEND GENERATED_FILES ${PARSED_NAME})
     endforeach()
 
-    set(OUT_DIR ${PROJECT_BINARY_DIR}/resources/${DIR})
-    set(OUT_QRC ${PROJECT_BINARY_DIR}/resources/${QRC}.qrc)
-    file(MAKE_DIRECTORY ${OUT_DIR})
-    set(PARSED_CPP ${PROJECT_BINARY_DIR}/${DIR}.cxx)
+
     set_source_files_properties(${GENERATED_FILES} PROPERTIES GENERATED TRUE)
-    set_source_files_properties(${PARSED_CPP} PROPERTIES GENERATED TRUE)
+    add_custom_target(${TARGET_NAME} COMMAND ${PARSER} ${IN_DIR} ${OUT_DIR} DEPENDS ${PARSER})
 
-    add_custom_target(parse_${DIR}_files COMMAND ${PARSER} ${DIR_PATH} ${OUT_DIR} DEPENDS ${PARSER})
-#    add_custom_target(parse_${DIR}_qrc COMMAND  ${CMAKE_COMMAND} -E copy ${QRC_PATH} ${CMAKE_BINARY_DIR}/resources DEPENDS parse_${DIR}_files)
-    add_custom_target(parse_${DIR} COMMAND ${Qt5Core_RCC_EXECUTABLE} ${rcc_options} -name ${QRC} -o ${PARSED_CPP} ${OUT_QRC} DEPENDS parse_${DIR}_files)
+endfunction(PARSE_RESOURCES_TARGET)
 
-endfunction(PARSE_QRC)
-
-function(COMPILE_QRC RESOURCE)
+function(COMPILE_QRC_TARGET RESOURCE)
     set(RESOURCE_CPP ${PROJECT_BINARY_DIR}/${RESOURCE}.cxx)
     set_source_files_properties(${RESOURCE_CPP} PROPERTIES GENERATED TRUE)
     add_custom_target(compile_${RESOURCE} COMMAND ${Qt5Core_RCC_EXECUTABLE} ${rcc_options} -name ${RESOURCE} -o ${RESOURCE_CPP} ${PROJECT_BINARY_DIR}/resources/${RESOURCE}.qrc)
-endfunction(COMPILE_QRC)
+endfunction(COMPILE_QRC_TARGET)
