9ad809acd08f3a5cfaa28df402d9717692394804
Victor Bucataru <ping@devktor.com>
2017-04-06 22:30:55 +0300

    optional test working_directory

--- Test.cmake
+++ Test.cmake
@@ -1,9 +1,15 @@
 include(${CMAKE_CURRENT_LIST_DIR}/Utils.cmake)
 
 function(ADD_TEST_TARGET TARGET_NAME)
-    ADD_TEST(build_${TARGET_NAME} "${CMAKE_COMMAND}" --build ${CMAKE_BINARY_DIR} --target ${TARGET_NAME})
-    ADD_TEST(NAME run_${TARGET_NAME} COMMAND ${TARGET_NAME})
-    SET_TESTS_PROPERTIES (run_${TARGET_NAME} PROPERTIES DEPENDS build_${TARGET_NAME})
+    add_test(build_${TARGET_NAME} "${CMAKE_COMMAND}" --build ${CMAKE_BINARY_DIR} --target ${TARGET_NAME})
+    list(LENGTH ARGN ARG_CNT)
+    if(ARG_CNT GREATER 0)
+        list(GET ARGN 0 WORKING_DIRECTORY)
+        add_test(NAME run_${TARGET_NAME} COMMAND ${TARGET_NAME} WORKING_DIRECTORY ${WORKING_DIRECTORY})
+    else()
+        add_test(NAME run_${TARGET_NAME} COMMAND ${TARGET_NAME})
+    endif()
+    set_tests_properties(run_${TARGET_NAME} PROPERTIES DEPENDS build_${TARGET_NAME})
 endfunction(ADD_TEST_TARGET)
 
 
@@ -23,25 +29,25 @@ function(GENERATE_QT_TEST_MAIN CPP_MAIN TEST_CASE_DIR)
 
 
     set(SOURCE_CODE "${INCLUDES}\n\
-template<class Test>                                                            \n\
-int ExecuteTest(int argc, char **argv)                                          \n\
-{                                                                               \n\
+template<class Test>                                                             \n\
+int ExecuteTest(int argc, char **argv)                                           \n\
+{                                                                                \n\
     Test test\;                                                                  \n\
     return QTest::qExec(&test, argc, argv)\;                                     \n\
-}                                                                               \n\
-                                                                                \n\
-#define EXECUTE_TEST(r, data, Test) ||ExecuteTest<Test>(argc, argv)             \n\
-#define RUN_TEST_SEQ(seq) 0 BOOST_PP_SEQ_FOR_EACH(EXECUTE_TEST,, seq )          \n\
-#define RUN_TESTS(...) RUN_TEST_SEQ(BOOST_PP_VARIADIC_TO_SEQ (__VA_ARGS__))     \n\
-int main(int argc, char** argv)                                                 \n\
-{                                                                               \n\
+}                                                                                \n\
+                                                                                 \n\
+#define EXECUTE_TEST(r, data, Test) ||ExecuteTest<Test>(argc, argv)              \n\
+#define RUN_TEST_SEQ(seq) 0 BOOST_PP_SEQ_FOR_EACH(EXECUTE_TEST,, seq )           \n\
+#define RUN_TESTS(...) RUN_TEST_SEQ(BOOST_PP_VARIADIC_TO_SEQ (__VA_ARGS__))      \n\
+int main(int argc, char** argv)                                                  \n\
+{                                                                                \n\
     int status = RUN_TESTS(${TEST_LIST})\;                                       \n\
-    if(status)                                                                  \n\
-    {                                                                           \n\
+    if(status)                                                                   \n\
+    {                                                                            \n\
         std::cout<<\"\\nUnit-Test failed\\n\"\;                                  \n\
-    }                                                                           \n\
+    }                                                                            \n\
     return status\;                                                              \n\
-}                                                                               \n\
+}                                                                                \n\
 ")
     file(WRITE ${CPP_MAIN} ${SOURCE_CODE})
 
