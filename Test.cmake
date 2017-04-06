df3f8d141280a6e65163ec7d4f97b0062abd03fd
Victor Bucataru <ping@devktor.com>
2017-04-06 11:35:53 +0300

    auto generate qt unit tests main.cpp

--- Test.cmake
+++ Test.cmake
@@ -1,3 +1,5 @@
+include(${CMAKE_CURRENT_LIST_DIR}/Utils.cmake)
+
 function(ADD_TEST_TARGET TARGET_NAME)
     ADD_TEST(build_${TARGET_NAME} "${CMAKE_COMMAND}" --build ${CMAKE_BINARY_DIR} --target ${TARGET_NAME})
     ADD_TEST(NAME run_${TARGET_NAME} COMMAND ${TARGET_NAME})
@@ -5,4 +7,50 @@ function(ADD_TEST_TARGET TARGET_NAME)
 endfunction(ADD_TEST_TARGET)
 
 
+function(GENERATE_QT_TEST_MAIN CPP_MAIN TEST_CASE_DIR)
+
+    file(GLOB_RECURSE TEST_FILES ${TEST_CASE_DIR}/*)
+    set(INCLUDES "#include <QTest>\n#include <iostream>\n#include <boost/preprocessor/variadic/to_seq.hpp>\n#include <boost/preprocessor/seq/for_each.hpp>")
+    set(TEST_LIST "")
+
+    foreach(TEST_FILE ${TEST_FILES})
+        set(INCLUDES "${INCLUDES}\n#include \"${TEST_FILE}\"")
+        get_filename_component(TEST_NAME ${TEST_FILE} NAME_WE)
+        message("test name " ${TEST_NAME})
+        list(APPEND TEST_LIST ${TEST_NAME})
+    endforeach()
+
+    JOIN("${TEST_LIST}" "," TEST_LIST)
+
+
+    set(SOURCE_CODE "${INCLUDES}\n\
+template<class Test>                                                            \n\
+int ExecuteTest(int argc, char **argv)                                          \n\
+{                                                                               \n\
+    Test test;                                                                  \n\
+    return QTest::qExec(&test, argc, argv);                                     \n\
+}                                                                               \n\
+                                                                                \n\
+#define EXECUTE_TEST(r, data, Test) ||ExecuteTest<Test>(argc, argv)             \n\
+#define RUN_TEST_SEQ(seq) 0 BOOST_PP_SEQ_FOR_EACH(EXECUTE_TEST,, seq )          \n\
+#define RUN_TESTS(...) RUN_TEST_SEQ(BOOST_PP_VARIADIC_TO_SEQ (__VA_ARGS__))     \n\
+int main(int argc, char** argv)                                                 \n\
+{                                                                               \n\
+    int status = RUN_TESTS(${TEST_LIST});                                       \n\
+    if(status)                                                                  \n\
+    {                                                                           \n\
+        std::cout<<\"\\nUnit-Test failed\\n\";                                  \n\
+    }                                                                           \n\
+    return status;                                                              \n\
+}                                                                               \n\
+")
+    file(WRITE ${CPP_MAIN} ${SOURCE_CODE})
+
+endfunction(GENERATE_QT_TEST_MAIN)
+
+
+
 enable_testing()
+
+
+
