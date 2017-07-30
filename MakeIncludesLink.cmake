6d9f974adbed9c2cae3ec16d088f2df0078c4f47
Victor Bucataru <ping@devktor.com>
2017-07-30 17:08:24 +0300

    using custom target

--- MakeIncludesLink.cmake
+++ MakeIncludesLink.cmake
@@ -1,3 +1,3 @@
 function (MakeIncludesLink)
-    add_custom_command(COMMAND ${CMAKE_COMMAND} -E create_symlink ${CMAKE_CURRENT_SOURCE_DIR}/src ${CMAKE_CURRENT_BINARY_DIR}/include/${PROJECT_NAME})
+    ADD_CUSTOM_TARGET(${PROJECT_NAME}_link_headers ALL COMMAND ${CMAKE_COMMAND} -E create_symlink ${CMAKE_CURRENT_SOURCE_DIR}/src ${CMAKE_CURRENT_BINARY_DIR}/include/${PROJECT_NAME})
 endfunction()
