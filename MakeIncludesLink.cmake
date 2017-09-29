9019865ae7b98fd9021f84bb9b433fe227b664f3
Victor Bucataru <ping@devktor.com>
2017-09-30 00:29:10 +0300

    creating include directory

--- MakeIncludesLink.cmake
+++ MakeIncludesLink.cmake
@@ -1,3 +1,4 @@
 function (MakeIncludesLink)
+    file(MAKE_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/include)
     execute_process(COMMAND ${CMAKE_COMMAND} -E create_symlink ${CMAKE_CURRENT_SOURCE_DIR}/src ${CMAKE_CURRENT_BINARY_DIR}/include/${PROJECT_NAME})
 endfunction()
