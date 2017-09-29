303c9a67a9e78b97f0dd6796e724a62318213f8a
Victor Bucataru <ping@devktor.com>
2017-09-30 00:11:33 +0300

    link headers before compilation

--- MakeIncludesLink.cmake
+++ MakeIncludesLink.cmake
@@ -1,3 +1,3 @@
 function (MakeIncludesLink)
-    ADD_CUSTOM_TARGET(${PROJECT_NAME}_link_headers ALL COMMAND ${CMAKE_COMMAND} -E create_symlink ${CMAKE_CURRENT_SOURCE_DIR}/src ${CMAKE_CURRENT_BINARY_DIR}/include/${PROJECT_NAME})
+    execute_process(COMMAND ${CMAKE_COMMAND} -E create_symlink ${CMAKE_CURRENT_SOURCE_DIR}/src ${CMAKE_CURRENT_BINARY_DIR}/include/${PROJECT_NAME})
 endfunction()
