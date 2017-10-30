78f12034fb633da0d16948860c489e3716027644
Victor Bucataru <ping@devktor.com>
2017-10-30 20:55:01 +0200

    parse arguments helper

--- Utils.cmake
+++ Utils.cmake
@@ -6,3 +6,17 @@ function(JOIN VALUES GLUE OUTPUT)
   string (REGEX REPLACE "[\\](.)" "\\1" _TMP_STR "${_TMP_STR}") #fixes escaping
   set (${OUTPUT} "${_TMP_STR}" PARENT_SCOPE)
 endfunction()
+
+
+
+function(PARSE_ARGUMENTS oneValueArgs multiValueArgs)
+
+    cmake_parse_arguments(ARG "" "${oneValueArgs}" "${multiValueArgs}" ${ARGN} )
+    foreach(VARIABLE_NAME ${oneValueArgs} ${multiValueArgs})
+        set(ARG_NAME ARG_${VARIABLE_NAME})
+        set(VARIABLE_VALUE ${${ARG_NAME}})
+        set(${VARIABLE_NAME} ${VARIABLE_VALUE})
+        set(${VARIABLE_NAME} ${VARIABLE_VALUE} PARENT_SCOPE)
+    endforeach()
+    
+endfunction()
